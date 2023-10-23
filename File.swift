0cde8e89a11ef9268d6a40ed754992f526a7f309
li3zhen1 <master_lee@live.com>
2023-10-23 19:47:23 -0400

    Add where clause transforming

--- File.swift
+++
@@ -1,8 +0,0 @@
-enum Scoped {
-    @WithSpecializedGeneric(namedAs: "Hola", specializing: "T", to: Int)
-    @WithSpecializedGeneric(namedAs: "Hej", specializing: "T", to: String)
-    class Hello<T, S>: Identifiable where T: Hashable, S.ID == T, S: Identifiable {
-        let id: T
-        let a: S
-    }
-}
