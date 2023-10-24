f9085b50af882ad03d29ad6a87b483cd2787ad35
li3zhen1 <master_lee@live.com>
2023-10-23 21:21:21 -0400

    Fix SpecializedSyntax

--- Sources/WithSpecializedGenericClient/main.swift
+++ Sources/WithSpecializedGenericClient/main.swift
@@ -1,25 +1,25 @@
 import WithSpecializedGeneric
 import simd
 
-protocol KDTreeDelegate {
-    associatedtype V: SIMD
-}
+// protocol KDTreeDelegate {
+//     associatedtype V: SIMD
+// }
 
-enum GreetingWords {
+// enum GreetingWords {
     
-    @WithSpecializedGeneric(namedAs: "Hej", specializing: "T", to: String)
-    class Hello<T, S>: Identifiable where T: Hashable, S.ID == T, S: Identifiable {
-        let id: T
-        let a: S
+//     @WithSpecializedGeneric(namedAs: "Hej", specializing: "T", to: String)
+//     class Hello<T, S>: Identifiable where T: Hashable, S.ID == T, S: Identifiable {
+//         let id: T
+//         let a: S
         
-        init(a: S) {
-            self.id = a.id
-            self.a = a
-        }
-    }
+//         init(a: S) {
+//             self.id = a.id
+//             self.a = a
+//         }
+//     }
     
-    @WithSpecializedGeneric(namedAs: "_QuadTree", specializing: "V", to: simd_double2)
-    public final class GenericTree<V, D> where V: SIMD, D: KDTreeDelegate, D.V == V {
-    }
+//     @WithSpecializedGeneric(namedAs: "_QuadTree", specializing: "V", to: simd_double2)
+//     public final class GenericTree<V, D> where V: SIMD, D: KDTreeDelegate, D.V == V {
+//     }
     
-}
+// }
